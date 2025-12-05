004aa1e2e804b57b8a689a0ac38ad1a5af0122d8
John <jiheredia1@my.scccd.edu>
2025-12-05 18:42:04 +0000

    lab 14.7 difference in thrusts

--- Heredia_14_7.m
+++ Heredia_14_7.m
@@ -1,1 +1,18 @@
+% Rocket Populsion
+% Plot the velocity of the rocket for the first 10 seconds for four values
+% Define the given constants
+m = 1000; % Kilograms
+c = 500; % Newtons
+v0 = 0; % m/s
 
+% Solve the differential equations with ode45
+for fi = 20000:20000:80000;
+[t,V] = ode45(@(t,V) (fi-c*V)/m, [0 10], v0);
+plot(t,V)
+grid on
+hold on
+end 
+legend('20,000N Thrust','40,000N Thrust','60,000N Thrust','80,000N Thrust', 'Location','Best')
+
+
+% ylim([0 80000])
